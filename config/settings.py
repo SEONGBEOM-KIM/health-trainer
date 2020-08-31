1e2dfab0cdca6d836f0a5a328ca213e6a487e4ca
SEONGBEOM-KIM <59245627+SEONGBEOM-KIM@users.noreply.github.com>
2020-08-31 18:59:36 +0900

    input ran password

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET")
+SECRET_KEY = os.environ.get("DJANGO_SECRET", "s#L@p@n2MQ*GLShp7Uk^b^gWbCw")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
